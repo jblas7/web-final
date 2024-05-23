a5ca1f534dba7e481166d4c034b3c73c12eb86a5
gaabsito <a23185@svalero.com>
2024-05-23 18:23:49 +0200

    cambios en desc. y añadidos nuevos

--- Base-Datos/inserts-productos.sql
+++ Base-Datos/inserts-productos.sql
@@ -39,82 +39,119 @@ VALUES (3, 'PECRANCHO POTATOES', 'Indulge in our delectable petal potatoes, perf
 
 
 INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
-VALUES (6, 'TABLESIDE PECAMOLE', 'Freshly made tortilla chips, and a molcajete brought to your table for you to enjoy a 
+VALUES (4, 'TABLESIDE PECAMOLE', 'Freshly made tortilla chips, and a molcajete brought to your table for you to enjoy a 
                             dream-worthy guacamole with avocado, pico de gallo, 
                             chili, and lime, all customized to your taste.', 14.50, '/IMG/', 1);
 
 INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
-VALUES (7, 'PECTINENTE BURGER', 'Our homemade brioche bun with 2 smash burgers, Oxaca, Monterrey, and
+VALUES (5, 'PECTINENTE BURGER', 'Our homemade brioche bun with 2 smash burgers, Oxaca, Monterrey, and
                             mozzarella cheese, crispy American bacon, pickles, tomato jam, and PECrancho sauce.', 14.60, '/IMG/', 2);
 
 INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
-VALUES (8, 'THE PECADORA', 'House-made brioche bun, double beef patty and cheddar cheese, shredded
+VALUES (6, 'THE PECADORA', 'House-made brioche bun, double beef patty and cheddar cheese, shredded
                             lettuce, all accompanied by the worlds most famous burger sauce containing pickles and
                             onions.', 14.80, '/IMG/', 2);
 
 INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
-VALUES (9, 'MC AND PECS BURGER', 'Our fabolus brioche bun with 2 smash burgers, pickles, and mac and cheese
+VALUES (7, 'MC AND PECS BURGER', 'Our fabolus brioche bun with 2 smash burgers, pickles, and mac and cheese
                             oozing from all sides. An irresistible combination!', 13.90, '/IMG/', 2);
 
 INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
-VALUES (10, 'PECLICIOSA BURGER', 'Brioche bun accompanied by 2 thin burgers with delicious cheddar cheese,
+VALUES (8, 'PECLICIOSA BURGER', 'Brioche bun accompanied by 2 thin burgers with delicious cheddar cheese,
                             pickles, and bacon—simple yet doubly satisfying.', 14.50, '/IMG/', 2);
 
 
 INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
-VALUES (12, 'PECEÑITA BURGER', 'Homemade brioche bun with Morucha and Ox beef, Raclette cheese
+VALUES (9, 'PECEÑITA BURGER', 'Homemade brioche bun with Morucha and Ox beef, Raclette cheese
                             breaded in panko, pickles, crispy bacon, and slightly spicy El Rancho sauce.', 15.90, '/IMG/', 3);
 
 
 INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
-VALUES (13, 'PECSCELENTE XXL', 'Brioche bun with a base of smoked barbecue, pickles, Morucha and Ox
+VALUES (10, 'PECSCELENTE XXL', 'Brioche bun with a base of smoked barbecue, pickles, Morucha and Ox
                             beef, cheddar cheese, slow-cooked Pulled Pork, and tempura onion.', 18.50, '/IMG/', 3);
 
 INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
-VALUES (14, 'PECDEROSA BURGER', 'Our homemade brioche bun accompanied by a Morucha and Ox beef burger, caramelized onions, bacon, cheddar cheese,
+VALUES (11, 'PECDEROSA BURGER', 'Our homemade brioche bun accompanied by a Morucha and Ox beef burger, caramelized onions, bacon, cheddar cheese,
  pickles, PECranch sauce, and right before your eyes, we heap on loads of bacon ash-infused mac 
  & cheese. Are you going to miss out on this?', 15.95, '/IMG/', 3);
 
 
 INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
-VALUES (15, 'LITTLE PECS BURGER', 'Our homemade brioche bun paired with two Morucha and Ox beef patties totaling 220g, 
+VALUES (12, 'LITTLE PECS BURGER', 'Our homemade brioche bun paired with two Morucha and Ox beef patties totaling 220g, 
 caramelized onions, bacon, pickles, cheddar cheese, PECranch sauce, and right in front of you,
  we pile on heaps of mac & cheese. An absolute madness!', 16.50, '/IMG/', 3);
 
 INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
-VALUES (16, 'BIG PECS BURGER', 'The crown jewel, our new Pretzel bun, 200-gram Morucha and Ox beef, 
+VALUES (13, 'BIG PECS BURGER', 'The crown jewel, our new Pretzel bun, 200-gram Morucha and Ox beef, 
 caramelized onions, Emmy sauce with a spicy kick, 
 crispy American bacon, and smoked cheese.', 20.90, '/IMG/', 3);
 
 INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
-VALUES (17, 'PECULIAR BURGER', 'Our new Pretzel bun, 200 grams of Morucha beef and ox meat, caramelized onions, 
+VALUES (14, 'PECULIAR BURGER', 'Our new Pretzel bun, 200 grams of Morucha beef and ox meat, caramelized onions, 
 Emmy sauce with a spicy kick, crispy American bacon, and smoked cheese.', 16.10, '/IMG/', 3);
 
 INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
-VALUES (18, 'PECARAMEL BURGER', 'Our foodie burger: 200-gram Morucha and Ox beef, served on our
+VALUES (15, 'PECARAMEL BURGER', 'Our foodie burger: 200-gram Morucha and Ox beef, served on our
                             homemade brioche bun with a nest of shoestring potatoes, cheddar cheese, crispy bacon,
                             cream, and Lotus biscuit crumble.', 16.90, '/IMG/', 3);
 
 INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
-VALUES (19, 'THE PECHUGERA', 'Our ultimate vegan burger comes with our homemade brioche bun, Beyond
+VALUES (16, 'THE PECHUGERA', 'Our ultimate vegan burger comes with our homemade brioche bun, Beyond
                             Meat patty, lettuce, tomato, pico de gallo, pickles, and barbecue sauce (add cheddar
                             cheese if desired).', 13.90, '/IMG/', 3);
 
+
+
+
 INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
-VALUES (20, 'FELIPEC SANDWICH', 'Feel presidential with our grilled high-quality beef striploin
+VALUES (17, 'FELIPEC SANDWICH', 'Feel presidential with our grilled high-quality beef striploin
                             sandwich, layered with sautéed peppers, onions, and melted cheddar cheese, all
                             topped with a smooth sauce and served on artisan brioche bread.', 15.50, '/IMG/', 4);
 
 INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
-VALUES (21, 'THE CHEESERO', 'The "Cheese Craze": Brioche bun with gooey melted cheddar,
+VALUES (18, 'THE CHEESERO', 'The "Cheese Craze": Brioche bun with gooey melted cheddar,
                             Monterrey Jack, and Manchego cheeses, topped with pickles and chipotle mayo.', 9.90, '/IMG/', 4);
 
+INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
+VALUES (19, 'THE CREAMY BACON BUTTER', 'Simply spectacular. Homemade brioche bread, edam cheese, smoked bacon meremelada,
+ smoked cheddar cheese, and to top it off, bacon crunchy to give that crunchy touch in every bite. Brutal.', 10.90, '/IMG/', 4);
 
 INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
-VALUES (22, 'LOTUS CHEESPEC', 'The Deliciously creamy cheesecake with Lotus cream and a Lotus biscuit base.', 7.50, '/IMG/', 5);
+VALUES (20, 'THE PECAMOLE BRIOCHE', 'A great and crazy sandwich, cradled in our brioche bread, with a creamy interior, 
+thanks to our homemade guacamole...a surprise in every bite!', 10.50, '/IMG/', 4);
+
+
+
 
 INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
-VALUES (23, 'HAPPY HIPO CHEESPEC', 'The Deliciously creamy cheesecake infused with Kinder cream and a touch of hippopotamus. What could possibly go wrong with this delightful mix?', 8.00, '/IMG/', 5);
+VALUES (21, 'THE PECHUGERA', 'Our ultimate vegan burger comes with our homemade brioche bun, Beyond
+                            Meat patty, lettuce, tomato, pico de gallo, pickles, and barbecue sauce (add cheddar
+                            cheese if desired).', 13.90, '/IMG/', 3);
 
 INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
-VALUES (24, 'TRADITIONAL CHEESPEC', 'The queen of the house, the classic cheesecake, creamy, sweet, with that cheesy flavor, simply spectacular.', 7.00, '/IMG/', 5);
+VALUES (22, 'THE PECHUGERA', 'Our ultimate vegan burger comes with our homemade brioche bun, Beyond
+                            Meat patty, lettuce, tomato, pico de gallo, pickles, and barbecue sauce (add cheddar
+                            cheese if desired).', 13.90, '/IMG/', 3);
+
+INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
+VALUES (23, 'THE PECHUGERA', 'Our ultimate vegan burger comes with our homemade brioche bun, Beyond
+                            Meat patty, lettuce, tomato, pico de gallo, pickles, and barbecue sauce (add cheddar
+                            cheese if desired).', 13.90, '/IMG/', 3);
+
+INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
+VALUES (24, 'THE PECHUGERA', 'Our ultimate vegan burger comes with our homemade brioche bun, Beyond
+                            Meat patty, lettuce, tomato, pico de gallo, pickles, and barbecue sauce (add cheddar
+                            cheese if desired).', 13.90, '/IMG/', 3);
+
+
+INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
+VALUES (25, 'LOTUS CHEESPEC', 'The Deliciously creamy cheesecake with Lotus cream and a Lotus biscuit base.', 7.50, '/IMG/', 5);
+
+INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
+VALUES (26, 'HAPPY HIPO CHEESPEC', 'The Deliciously creamy cheesecake infused with Kinder cream and a touch of hippopotamus. What could possibly go wrong with this delightful mix?', 8.00, '/IMG/', 5);
+
+INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
+VALUES (27, 'TRADITIONAL CHEESPEC', 'The queen of the house, the classic cheesecake, creamy, sweet, with that cheesy flavor, simply spectacular.', 7.00, '/IMG/', 5);
+
+INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
+VALUES (28, 'TRADITIONAL CHEESPEC', 'The queen of the house, the classic cheesecake, creamy, sweet, with that cheesy flavor, simply spectacular.', 7.00, '/IMG/', 5);
