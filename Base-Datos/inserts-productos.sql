1669b79cd9d633e4f3fd215e3e950e3b9b0f6195
gaabsito <a23185@svalero.com>
2024-05-23 18:41:17 +0200

    añadidos todos los productos (BUENO)

--- Base-Datos/inserts-productos.sql
+++ Base-Datos/inserts-productos.sql
@@ -10,16 +10,16 @@ INSERT INTO Categoria (ID_Categoria, Nombre)
 VALUES(3, 'MEDALLION');
 
 INSERT INTO Categoria (ID_Categoria, Nombre)
-VALUES(3, 'SAUCES');
-
-INSERT INTO Categoria (ID_Categoria, Nombre)
 VALUES(4, 'SANDWITCHES');
 
 INSERT INTO Categoria (ID_Categoria, Nombre)
-VALUES(5, 'DESSERTS');
+VALUES(5, 'SAUCES');
 
 INSERT INTO Categoria (ID_Categoria, Nombre)
-VALUES(5, 'DRINKS');
+VALUES(6, 'DESSERTS');
+
+INSERT INTO Categoria (ID_Categoria, Nombre)
+VALUES(7, 'DRINKS');
 
 	/* INSERTS PARA PRODUCTOS */
 
@@ -37,11 +37,11 @@ VALUES (3, 'PECRANCHO POTATOES', 'Indulge in our delectable petal potatoes, perf
                             mac and cheese, succulent fried chicken, and a drizzle of tangy barbecue sauce for an
                             unforgettable culinary experience.', 11.90, '/IMG/', 1);
 
-
 INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
 VALUES (4, 'TABLESIDE PECAMOLE', 'Freshly made tortilla chips, and a molcajete brought to your table for you to enjoy a 
                             dream-worthy guacamole with avocado, pico de gallo, 
                             chili, and lime, all customized to your taste.', 14.50, '/IMG/', 1);
+                        
 
 INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
 VALUES (5, 'PECTINENTE BURGER', 'Our homemade brioche bun with 2 smash burgers, Oxaca, Monterrey, and
@@ -124,34 +124,65 @@ thanks to our homemade guacamole...a surprise in every bite!', 10.50, '/IMG/', 4
 
 
 INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
-VALUES (21, 'THE PECHUGERA', 'Our ultimate vegan burger comes with our homemade brioche bun, Beyond
-                            Meat patty, lettuce, tomato, pico de gallo, pickles, and barbecue sauce (add cheddar
-                            cheese if desired).', 13.90, '/IMG/', 3);
+VALUES (21, 'BOURBON BBQ SAUCE', '', 0.90, '/IMG/', 5);
 
 INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
-VALUES (22, 'THE PECHUGERA', 'Our ultimate vegan burger comes with our homemade brioche bun, Beyond
-                            Meat patty, lettuce, tomato, pico de gallo, pickles, and barbecue sauce (add cheddar
-                            cheese if desired).', 13.90, '/IMG/', 3);
+VALUES (22, 'CHIPOTLE SAUCE', '', 0.90, '/IMG/', 5);
 
 INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
-VALUES (23, 'THE PECHUGERA', 'Our ultimate vegan burger comes with our homemade brioche bun, Beyond
-                            Meat patty, lettuce, tomato, pico de gallo, pickles, and barbecue sauce (add cheddar
-                            cheese if desired).', 13.90, '/IMG/', 3);
+VALUES (23, 'SMOKED MAYO SAUCE', '', 0.90, '/IMG/', 5);
 
 INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
-VALUES (24, 'THE PECHUGERA', 'Our ultimate vegan burger comes with our homemade brioche bun, Beyond
-                            Meat patty, lettuce, tomato, pico de gallo, pickles, and barbecue sauce (add cheddar
-                            cheese if desired).', 13.90, '/IMG/', 3);
+VALUES (24, 'WASABI TOP SAUCE', '', 0.90, '/IMG/', 5);
+
 
 
 INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
-VALUES (25, 'LOTUS CHEESPEC', 'The Deliciously creamy cheesecake with Lotus cream and a Lotus biscuit base.', 7.50, '/IMG/', 5);
+VALUES (25, 'LOTUS CHEESPEC', 'The Deliciously creamy cheesecake with Lotus cream and a Lotus biscuit base.', 7.50, '/IMG/', 6);
 
 INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
-VALUES (26, 'HAPPY HIPO CHEESPEC', 'The Deliciously creamy cheesecake infused with Kinder cream and a touch of hippopotamus. What could possibly go wrong with this delightful mix?', 8.00, '/IMG/', 5);
+VALUES (26, 'HAPPY HIPO CHEESPEC', 'The Deliciously creamy cheesecake infused with Kinder cream and a touch of hippopotamus. What could possibly go wrong with this delightful mix?', 8.45, '/IMG/', 6);
 
 INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
-VALUES (27, 'TRADITIONAL CHEESPEC', 'The queen of the house, the classic cheesecake, creamy, sweet, with that cheesy flavor, simply spectacular.', 7.00, '/IMG/', 5);
+VALUES (27, 'TRADITIONAL CHEESPEC', 'The queen of the house, the classic cheesecake, creamy, sweet, with that cheesy flavor, simply spectacular.', 7.45, '/IMG/', 6);
 
 INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
-VALUES (28, 'TRADITIONAL CHEESPEC', 'The queen of the house, the classic cheesecake, creamy, sweet, with that cheesy flavor, simply spectacular.', 7.00, '/IMG/', 5);
+VALUES (28, 'CHOCO BROWNIE', 'Another great option, a chocolate brownie with nuts, accompanied by an almond nougat ice cream, moist, creamy ... delicious.', 6.45, '/IMG/', 6);
+
+
+
+INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
+VALUES (29, 'COCA-COLA® ORIGINAL', '', 1.90, '/IMG/', 7);
+
+INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
+VALUES (30, 'COCA‑COLA® ZERO', '', 1.90, '/IMG/', 7);
+
+INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
+VALUES (31, 'FANTA® ORANGE', '', 1.90, '/IMG/', 7);
+
+INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
+VALUES (32, 'AQUARIUS® LEMON', '', 1.90, '/IMG/', 7);
+
+INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
+VALUES (33, 'NESTEA®', '', 1.90, '/IMG/', 7);
+
+INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
+VALUES (34, 'SPRITE®', '', 1.90, '/IMG/', 7);
+
+INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
+VALUES (35, 'WATER', '', 1.40, '/IMG/', 7);
+
+INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
+VALUES (36, 'BEER', '', 2.10, '/IMG/', 7);
+
+INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
+VALUES (37, 'RED BULL ORIGINAL', '', 2.40, '/IMG/', 7);
+
+INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
+VALUES (38, 'RED BULL SUGARFREE', '', 2.40, '/IMG/', 7);
+
+INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
+VALUES (39, 'RED BULL WATERMELON', '', 2.40, '/IMG/', 7);
+
+INSERT INTO Productos (ID_Producto, Nombre, Descripcion, Precio, Ruta_Imagen, ID_Categoria)
+VALUES (40, 'RED BULL APRICOT', '', 2.40, '/IMG/', 7);
