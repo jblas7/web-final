a5c8764afef87f0b2bd9657a0bf637b447d2d3b5
gaabsito <a23185@svalero.com>
2024-05-27 17:01:04 +0200

    UPDATE FINAL BBDD

--- Base-Datos/BBDD.sql
+++ Base-Datos/BBDD.sql
@@ -43,7 +43,7 @@ CREATE TABLE Productos(
 CREATE TABLE Pedidos (
     ID_Pedido INTEGER PRIMARY KEY,
     Tipo_Pedido VARCHAR2(20) NOT NULL, 
-    Modo_Entrega VARCHAR2(20),
+    Modo_Entrega VARCHAR2(20),/*MIRAR A VER*/
     Hora_Entrega VARCHAR2(10), 
     Shop VARCHAR2(50), 
     Modo_Recoger VARCHAR2(20),
