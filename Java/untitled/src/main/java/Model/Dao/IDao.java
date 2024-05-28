8fe89aa6921d40c774652d5eff5234edb7d2434c
Jorge <118572558+jblas7@users.noreply.github.com>
2024-05-28 19:56:48 +0200

    arreglo sql exception

--- Java/untitled/src/main/java/Model/Dao/IDao.java
+++ Java/untitled/src/main/java/Model/Dao/IDao.java
@@ -1,9 +1,10 @@
 package Model.Dao;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 
     public interface IDao<E,I> {
-        public int add(E e);
+        public int add(E e) throws SQLException;
         public int delete(Integer e);
         public int update(E e);
         public ArrayList<E> findAll(E e);
